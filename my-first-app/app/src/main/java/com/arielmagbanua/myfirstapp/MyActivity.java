6596771caef810c384141fc27d445b277266eaf6
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2018-07-21 08:54:51 +0800

    Added challenge layouts.

--- my-first-app/app/src/main/java/com/arielmagbanua/myfirstapp/MyActivity.java
+++ my-first-app/app/src/main/java/com/arielmagbanua/myfirstapp/MyActivity.java
@@ -8,6 +8,6 @@ public class MyActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_my);
+        setContentView(R.layout.challenge_relative_layout);
     }
 }
