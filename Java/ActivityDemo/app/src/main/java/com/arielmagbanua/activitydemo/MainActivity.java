cf937b44846eabc3c92278b618bba971daf2793a
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2020-06-01 13:50:51 +0800

    Use the androidX version for AppCompatActivity

--- Java/ActivityDemo/app/src/main/java/com/arielmagbanua/activitydemo/MainActivity.java
+++ Java/ActivityDemo/app/src/main/java/com/arielmagbanua/activitydemo/MainActivity.java
@@ -1,13 +1,12 @@
 package com.arielmagbanua.activitydemo;
 
 import android.content.Intent;
-import android.os.PersistableBundle;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
     public static final String TAG = "MainActivity";
