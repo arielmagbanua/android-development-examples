e7d75c18c583ab67752764e52e4fe52a8cbc7537
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2020-06-01 13:54:09 +0800

    Used the androidX library.

--- Java/ActivityDemo/app/src/main/java/com/arielmagbanua/activitydemo/SecondActivity.java
+++ Java/ActivityDemo/app/src/main/java/com/arielmagbanua/activitydemo/SecondActivity.java
@@ -1,7 +1,7 @@
 package com.arielmagbanua.activitydemo;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class SecondActivity extends AppCompatActivity {
 
